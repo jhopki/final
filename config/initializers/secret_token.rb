3e7101365e79ad11e21e4f98eaa230db451c25d2
Julia Hopkins <jfhopkins6@gmail.com>
2014-01-19 21:39:35 -0500

    fixed small issue

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Superforum::Application.config.secret_key_base = ENV['SECRET_KET_BASE']
+Superforum::Application.config.secret_key_base = '1aeed9d6bfa6b56fd5ef43d69f3f25d92d4f650ee95f90dd06e0025b7e9c62995b32d42d498d421fd4e2127a7c5b95edf08aeb0f3d79b34efaf04458f610730f'
