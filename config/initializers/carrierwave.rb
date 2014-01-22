31803eba73059bb186c7fd68cdc8b9fc4d2b9d95
Julia Hopkins <jfhopkins6@gmail.com>
2014-01-22 15:46:09 -0500

    changed uploader

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      =>  ENV['#{ENV["S3_KEY"]}'],
-    :aws_secret_access_key  =>  ENV['#{ENV["S3_SECRET"]}'],
+    :aws_access_key_id      =>  ENV['S3_KEY'],
+    :aws_secret_access_key  =>  ENV['S3_SECRET'],
     :region					=>	ENV['S3_REGION']
   }
 
