2c17af97d561afb46f553154eb91b411da38bb31
Julia Hopkins <jfhopkins6@gmail.com>
2014-01-22 19:00:50 -0500

    changes 6

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,9 +22,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     # 'public/transresourcestoronto'
-    'https://transresourcestoronto.s3.amazonaws.com/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}'
+    # 'https://transresourcestoronto.s3.amazonaws.com/users/#{user.id}/items'
   end
 
   def extension_white_list
